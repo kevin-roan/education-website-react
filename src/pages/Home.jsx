b23d4e18d5aa6c2fc996f3c909c556b356b20dbf
Kevin Roan <kevinroan0303@gmail.com>
2023-11-11 16:04:25 +0530

    minor changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,16 +3,14 @@ import "./Home.scss";
 function Home() {
   return (
     <div className="home-container">
-      <div className="home-content">
-        <h1>Let Your Dreams Take Flight</h1>
-        <p>
-          Yugen School of Accounting & Taxation aims to provide world class
-          training for Global Accounting and Taxation qualifications and develop
-          the professional Accountant of upcoming Days.
-        </p>
-        <button className="home-button">Know More</button>
-        <button className="home-button">Download Brochure</button>
-      </div>
+      <h1>Let Your Dreams Take Flight</h1>
+      <p>
+        Yugen School of Accounting & Taxation aims to provide world class
+        training for Global Accounting and Taxation qualifications and develop
+        the professional Accountant of upcoming Days.
+      </p>
+      <button className="home-button">Know More</button>
+      <button className="home-button">Download Brochure</button>
     </div>
   );
 }
