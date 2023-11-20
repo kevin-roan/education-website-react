1928d07139c61f5e2524588f45565532e2f98303
Kevin Roan <kevinroan0303@gmail.com>
2023-11-20 17:26:43 +0530

    fix responsiveness

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -17,6 +17,18 @@ const galleryImages = [
     title: "image4",
     path: "https://images.pexels.com/photos/3184292/pexels-photo-3184292.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
   },
+  {
+    title: "image4",
+    path: "https://images.pexels.com/photos/3184292/pexels-photo-3184292.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+  {
+    title: "image1",
+    path: "https://images.pexels.com/photos/1709003/pexels-photo-1709003.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+  {
+    title: "image2",
+    path: "https://images.pexels.com/photos/1595385/pexels-photo-1595385.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
 ];
 
 function Gallery() {
